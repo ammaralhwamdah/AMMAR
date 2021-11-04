da4697482e5e1223162f4d34ff042883b0cb45a9
ammaralhwamdah <93740441+ammaralhwamdah@users.noreply.github.com>
2021-11-04 23:53:59 +0200

    Create conversion-page.js

---
+++ conversion-page.js
@@ -0,0 +1,28 @@
+<script type='text/javascript'>
+ //<![CDATA[
+var allahref=document.getElementsByTagName("a");
+for(var c=0;c<allahref.length;c++)
+{
+if(allahref[c].hostname!="download-free-games-hd.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.facebook.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="web.facebook.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.youtube.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="pinterest.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.pinterest.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="twitter.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.twitter.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="1.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="2.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="3.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="4.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.blogger.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.vk.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="vk.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="play.google.com" && allahref[c].hostname!="")
+{
+allahref[c].href="https://download-free-games-hd.blogspot.com/p/areload-display-nonearabes1-background_29.html??&&url=_"+allahref[c].href;
+}
+}
+ //]]>
+</script>  
